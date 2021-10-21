0e85a5335b8b7213a32b93aaa966165179099edb
achmadrizkin <arizki.nf02@gmail.com>
2021-10-21 14:39:40 +0700

    add splash screen

---
+++ lib/pages/splash_screen.dart
@@ -0,0 +1,62 @@
+import 'package:auto_size_text_pk/auto_size_text_pk.dart';
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:flutter/services.dart';
+import 'package:flutter_ecommerce_laravel/pages/bottom_navigation.dart';
+import 'package:flutter_ecommerce_laravel/utils/color.dart';
+import 'package:flutter_ecommerce_laravel/utils/text_style.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    Timer(Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacement(MaterialPageRoute(
+        builder: (context) => BottomNav(),
+      ));
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(child: Image.asset("asset/logo.png")),
+          AutoSizeText(
+            "Flutter E-Commerce",
+            style: headingStyle.copyWith(
+                fontSize: 24, color: white, fontWeight: FontWeight.bold),
+          ),
+          AutoSizeText(
+            "created by: Achmad Rizki Nur Fauzie",
+            style: headingStyle.copyWith(
+                fontSize: 14,
+                color: grey,
+                fontWeight: FontWeight.normal),
+          ),
+        ],
+      ),
+    );
+  }
+}
