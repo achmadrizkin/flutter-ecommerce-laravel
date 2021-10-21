91d7a21c729316af00019a6677cf4fe125961478
achmadrizkin <arizki.nf02@gmail.com>
2021-10-21 14:39:32 +0700

    add botom navigation

---
+++ lib/pages/bottom_navigation.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce_laravel/pages/home/home.dart';
+import 'package:flutter_ecommerce_laravel/pages/search/search.dart';
+import 'package:flutter_ecommerce_laravel/pages/user/user.dart';
+import 'package:flutter_ecommerce_laravel/utils/color.dart';
+
+class BottomNav extends StatefulWidget {
+  const BottomNav({Key? key}) : super(key: key);
+
+  @override
+  State<BottomNav> createState() => _BottomNavState();
+}
+
+/// This is the private State class that goes with BottomNav.
+class _BottomNavState extends State<BottomNav> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    Home(),
+    Search(),
+    User(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Me',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: white,
+        unselectedItemColor: unselectedItem,
+        backgroundColor: bottomNavigation,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
