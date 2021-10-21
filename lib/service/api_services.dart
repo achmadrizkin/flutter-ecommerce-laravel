6230ebe9c11af2ea0042770409c943388e39c74a
achmadrizkin <arizki.nf02@gmail.com>
2021-10-21 20:03:41 +0700

    .

--- lib/service/api_services.dart
+++ lib/service/api_services.dart
@@ -6,13 +6,26 @@ import 'package:http/http.dart' as http;
 class ApiServices {
   // get data from API
   Future<Barang> getData() async {
-    final response = await http
-        .get(Uri.parse('http://10.0.2.2:8001/api/products'));
+    final response =
+        await http.get(Uri.parse('http://10.0.2.2:8001/api/products'));
 
     if (response.statusCode == 200) {
       return Barang.fromJson(jsonDecode(response.body));
     } else {
       throw Exception();
     }
+  }
+
+  Future saveProduct(
+      String name, String description, String imageURL, String price) async {
+    final response =
+        await http.post(Uri.parse('http://10.0.2.2:8001/api/products'), body: {
+      "name": name,
+      "description": description,
+      "price": price,
+      "image_url": imageURL,
+    });
+
+    return jsonDecode(response.body);
   }
 }
