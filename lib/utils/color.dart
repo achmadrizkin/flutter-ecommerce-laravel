d1ed8ef5882a13c95b2fd09c02b36533f60860a9
achmadrizkin <arizki.nf02@gmail.com>
2021-10-21 14:39:12 +0700

    add color

---
+++ lib/utils/color.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const Color bluishClr = Color(0xFF4e5ae8);
+const Color yellowClr = Color(0xFFFFB746);
+const Color pinkClr = Color(0xFFff4667);
+
+const Color white = Colors.white;
+const primaryClr = bluishClr;
+
+const Color darkGreyClr = Color(0xFF121212);
+const Color darkHeader = Color(0xFF424242);
+const Color black = Colors.black;
+
+const Color unselectedItem = Color(0xFFb8b8b8);
+const Color bottomNavigation = Color(0xFF282828);
+const Color brownClr = Color(0xFF42393c);
+const Color grey = Colors.grey;
