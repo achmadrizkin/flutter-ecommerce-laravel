a8f47359c6dfce6bbb85f56408f65432eac13ccd
achmadrizkin <arizki.nf02@gmail.com>
2021-10-22 10:46:15 +0700

    a

--- lib/pages/bottom_navigation.dart
+++ lib/pages/bottom_navigation.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce_laravel/pages/connection/no_internet.dart';
 import 'package:flutter_ecommerce_laravel/pages/home/home.dart';
 import 'package:flutter_ecommerce_laravel/pages/search/search.dart';
 import 'package:flutter_ecommerce_laravel/pages/user/user.dart';
+import 'package:flutter_ecommerce_laravel/provider/connectivity_provider.dart';
 import 'package:flutter_ecommerce_laravel/utils/color.dart';
+import 'package:provider/provider.dart';
 
 class BottomNav extends StatefulWidget {
   const BottomNav({Key? key}) : super(key: key);
@@ -23,6 +26,12 @@ class _BottomNavState extends State<BottomNav> {
     User(),
   ];
 
+  @override
+  void initState() {
+    super.initState();
+    Provider.of<ConnectivityProvider>(context, listen: false).startMonitoring();
+  }
+
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
@@ -30,37 +39,44 @@ class _BottomNavState extends State<BottomNav> {
   }
 
   @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: 'Search',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Me',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: white,
-        unselectedItemColor: unselectedItem,
-        backgroundColor: bottomNavigation,
-        onTap: _onItemTapped,
-      ),
+    return Consumer<ConnectivityProvider>(
+      builder: (context, model, child) {
+        if (model.isOnline != null) {
+          return model.isOnline
+              ? Scaffold(
+                  body: Center(
+                    child: _widgetOptions.elementAt(_selectedIndex),
+                  ),
+                  bottomNavigationBar: BottomNavigationBar(
+                    items: const <BottomNavigationBarItem>[
+                      BottomNavigationBarItem(
+                        icon: Icon(Icons.home),
+                        label: 'Home',
+                      ),
+                      BottomNavigationBarItem(
+                        icon: Icon(Icons.search),
+                        label: 'Search',
+                      ),
+                      BottomNavigationBarItem(
+                        icon: Icon(Icons.library_books),
+                        label: 'Your Library',
+                      ),
+                    ],
+                    currentIndex: _selectedIndex,
+                    selectedItemColor: white,
+                    unselectedItemColor: unselectedItem,
+                    backgroundColor: bottomNavigation,
+                    onTap: _onItemTapped,
+                  ),
+                )
+              : NoInternetConnection();
+        }
+
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+      },
     );
   }
 }
