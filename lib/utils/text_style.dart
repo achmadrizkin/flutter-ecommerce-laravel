e734b3d989277fb5a2fb1663055fe069c713a6d7
achmadrizkin <arizki.nf02@gmail.com>
2021-10-22 11:19:23 +0700

    a

--- lib/utils/text_style.dart
+++ lib/utils/text_style.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce_laravel/utils/color.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 TextStyle get headingStyle {
   return GoogleFonts.poppins(
     textStyle: TextStyle(fontWeight: FontWeight.bold),
-    color: Colors.white,
+    color: white,
   );
 }
 
 TextStyle get headingStyle2 {
   return GoogleFonts.poppins(
     textStyle: TextStyle(fontWeight: FontWeight.w600),
-    color: Colors.white,
+    color: white,
   );
 }
 
@@ -20,5 +21,6 @@ TextStyle get subTitleTextStyle {
   return GoogleFonts.poppins(
     textStyle: TextStyle(fontWeight: FontWeight.w400),
     fontSize: 14,
+    color: grey,
   );
 }
