35e17b5ca0a5c1ed0e6ea32cfeabcaef7040bc1d
achmadrizkin <arizki.nf02@gmail.com>
2021-10-23 10:52:24 +0700

    j

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,6 +1,6 @@
 import 'package:auto_size_text_pk/auto_size_text_pk.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_ecommerce_laravel/model/products_model.dart';
+import 'package:flutter_ecommerce_laravel/pages/cart/cart_product.dart';
 import 'package:flutter_ecommerce_laravel/pages/home/widget/add_product.dart';
 import 'package:flutter_ecommerce_laravel/pages/home/widget/all_products.dart';
 import 'package:flutter_ecommerce_laravel/utils/color.dart';
@@ -17,10 +17,21 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       backgroundColor: black,
       appBar: AppBar(
         backgroundColor: black,
         title: Text("E-Commerce Shop"),
+        actions: [
+          IconButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => CartProducts()),
+                );
+              },
+              icon: Icon(Icons.shopping_cart))
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -33,11 +44,18 @@ class _HomeState extends State<Home> {
       ),
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.only(left: 10.0, right: 10),
+          padding: const EdgeInsets.only(left: 10.0, right: 10, top: 10),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              AutoSizeText(
+                "All Product",
+                style: headingStyle.copyWith(fontSize: 20),
+              ),
+              AllProducts(),
+
+              //
               AutoSizeText(
                 "All Product",
                 style: headingStyle.copyWith(fontSize: 20),
