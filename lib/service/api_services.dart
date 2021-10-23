9d756f0c3240efa45457c3440095582c78ca3e6f
achmadrizkin <arizki.nf02@gmail.com>
2021-10-23 11:23:54 +0700

    a

--- lib/service/api_services.dart
+++ lib/service/api_services.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter_ecommerce_laravel/model/get_cart_model.dart';
+import 'package:flutter_ecommerce_laravel/model/get_products.dart';
 import 'package:flutter_ecommerce_laravel/model/products_model.dart';
 import 'package:http/http.dart' as http;
 
@@ -56,7 +57,7 @@ class ApiServices {
     return jsonDecode(response.body);
   }
 
- Future<List<GetCart>> getMusic(String query) async {
+ Future<List<GetCart>> getCartByUser(String query) async {
     final response = await http
         .get(Uri.parse('https://achmadrizkin.my.id/flutter-store-app/php/select_cart.php'));
 
@@ -64,6 +65,23 @@ class ApiServices {
       final List favorite = json.decode(response.body);
       return favorite.map((json) => GetCart.fromJson(json)).where((favorite) {
         final favoriteLower = favorite.user.toLowerCase();
+        final searchLower = query.toLowerCase();
+
+        return favoriteLower.contains(searchLower);
+      }).toList();
+    } else {
+      throw Exception();
+    }
+  }
+
+  Future<List<GetProduct>> getByProduct(String query) async {
+    final response = await http
+        .get(Uri.parse('https://achmadrizkin.my.id/flutter-store-app/php/select_flashdisk.php'));
+
+    if (response.statusCode == 200) {
+      final List favorite = json.decode(response.body);
+      return favorite.map((json) => GetProduct.fromJson(json)).where((favorite) {
+        final favoriteLower = favorite.name.toLowerCase();
         final searchLower = query.toLowerCase();
 
         return favoriteLower.contains(searchLower);
