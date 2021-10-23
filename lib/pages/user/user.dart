7275551bb6a34ef228bbdba0ecb7a71d88d4dac3
achmadrizkin <arizki.nf02@gmail.com>
2021-10-23 10:52:03 +0700

    a

--- lib/pages/user/user.dart
+++ lib/pages/user/user.dart
@@ -1,5 +1,6 @@
 import 'package:auto_size_text_pk/auto_size_text_pk.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce_laravel/pages/user/widget/user_detail.dart';
 import 'package:flutter_ecommerce_laravel/utils/color.dart';
 import 'package:flutter_ecommerce_laravel/utils/text_style.dart';
 
@@ -15,45 +16,15 @@ class User extends StatelessWidget {
       backgroundColor: black,
       body: SafeArea(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Container(
-              width: screenWidth,
-              height: screenHeight / 6,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: const Radius.circular(40.0),
-                    bottomRight: const Radius.circular(40.0),
-                  ),
-                  color: white),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  ClipOval(
-                    child: Image.network(
-                        "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aHVtYW58ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80",
-                        fit: BoxFit.cover,
-                        width: 80,
-                        height: 80),
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      AutoSizeText("Achmad Rizki Nur F",
-                          style: headingStyle.copyWith(
-                            color: black,
-                          )),
-                      AutoSizeText(
-                        "arizki.nf02@gmail.com",
-                        style: subTitleTextStyle.copyWith(color: grey),
-                      ),
-                    ],
-                  ),
-                  IconButton(
-                    icon: Icon(Icons.favorite),
-                    onPressed: () {},
-                  ),
-                ],
+            UserDetails(screenWidth: screenWidth, screenHeight: screenHeight),
+            SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.only(left: 10.0, right: 10),
+              child: AutoSizeText(
+                "Your Favorite",
+                style: headingStyle.copyWith(fontSize: 20),
               ),
             ),
           ],
