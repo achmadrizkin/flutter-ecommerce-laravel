324b47cee84bcf5716a483ba74589d6563fb4f0b
achmadrizkin <arizki.nf02@gmail.com>
2021-10-24 10:13:31 +0700

    d

--- lib/pages/home/widget/product_brand.dart
+++ lib/pages/home/widget/product_brand.dart
@@ -9,6 +9,7 @@ import 'package:flutter_ecommerce_laravel/pages/product_details/product_getProdu
 import 'package:flutter_ecommerce_laravel/service/api_services.dart';
 import 'package:flutter_ecommerce_laravel/utils/color.dart';
 import 'package:flutter_ecommerce_laravel/utils/text_style.dart';
+import 'package:flutter_shimmer/flutter_shimmer.dart';
 
 class ProductBrand extends StatefulWidget {
   const ProductBrand({Key? key}) : super(key: key);
@@ -71,69 +72,85 @@ class _ProductBrandState extends State<ProductBrand> {
       padding: const EdgeInsets.only(left: 10.0),
       child: FutureBuilder<List<GetProduct>>(
         future: ApiServices().getByProduct(query),
-        builder: (context, index) {
-          return SizedBox(
-            width: MediaQuery.of(context).size.width - 20,
-            height: MediaQuery.of(context).size.height / 4,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: product.length,
-              itemBuilder: (context, index) {
-                final data = product[index];
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return SizedBox(
+              width: MediaQuery.of(context).size.width - 20,
+              height: MediaQuery.of(context).size.height / 4,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: product.length,
+                itemBuilder: (context, index) {
+                  final data = product[index];
 
-                return Container(
-                  color: black,
-                  child: InkWell(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => ProductGetProductDetails(data: data,)),
-                      );
-                    },
-                    child: Container(
-                      width: MediaQuery.of(context).size.width / 2 - 40,
-                      height: MediaQuery.of(context).size.height / 2 - 10,
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15.0),
-                        color: black,
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            width: 120,
-                            height: 120,
-                            decoration: BoxDecoration(),
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(20),
-                              child: Image.network(
-                                data.imageUrl,
-                                fit: BoxFit.cover,
+                  return Container(
+                    color: black,
+                    child: InkWell(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => ProductGetProductDetails(
+                                    data: data,
+                                  )),
+                        );
+                      },
+                      child: Container(
+                        width: MediaQuery.of(context).size.width / 2 - 40,
+                        height: MediaQuery.of(context).size.height / 2 - 10,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15.0),
+                          color: black,
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              width: 120,
+                              height: 120,
+                              decoration: BoxDecoration(),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(20),
+                                child: Image.network(
+                                  data.imageUrl,
+                                  fit: BoxFit.cover,
+                                ),
                               ),
                             ),
-                          ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          AutoSizeText(data.name,
-                              style: headingStyle2.copyWith(
-                                  color: Colors.white,
-                                  overflow: TextOverflow.ellipsis)),
-                          AutoSizeText("\$ ${data.price}",
-                              style: subTitleTextStyle.copyWith(
-                                  color: Colors.grey,
-                                  fontSize: 5,
-                                  overflow: TextOverflow.ellipsis)),
-                        ],
+                            SizedBox(
+                              height: 10,
+                            ),
+                            AutoSizeText(data.name,
+                                style: headingStyle2.copyWith(
+                                    color: Colors.white,
+                                    overflow: TextOverflow.ellipsis)),
+                            AutoSizeText("\$ ${data.price}",
+                                style: subTitleTextStyle.copyWith(
+                                    color: Colors.grey,
+                                    fontSize: 5,
+                                    overflow: TextOverflow.ellipsis)),
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                );
-              },
-            ),
-          );
+                  );
+                },
+              ),
+            );
+          } else {
+            return PlayStoreShimmer(
+              hasCustomColors: true,
+              colors: [
+                grey,
+                // light color
+                grey,
+                // Medium color
+                grey,
+              ],
+            );
+          }
         },
       ),
     );
