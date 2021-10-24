a30325a8e51ce9acea4cb4418b4302f95d644eba
achmadrizkin <arizki.nf02@gmail.com>
2021-10-24 17:03:21 +0700

    a

--- lib/model/get_products.dart
+++ lib/model/get_products.dart
@@ -1,38 +1,57 @@
 // To parse this JSON data, do
 //
-//     final getProduct = getProductFromJson(jsonString);
+//     final GetProduct = GetProductsFromJson(jsonString);
 
 import 'dart:convert';
 
-List<GetProduct> getProductFromJson(String str) =>
-    List<GetProduct>.from(json.decode(str).map((x) => GetProduct.fromJson(x)));
+List<GetProduct> GetProductsFromJson(String str) => List<GetProduct>.from(json.decode(str).map((x) => GetProduct.fromJson(x)));
+
+String GetProductsToJson(List<GetProduct> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
 class GetProduct {
-  GetProduct({
-    required this.id,
-    required this.name,
-    required this.description,
-    required this.price,
-    required this.imageUrl,
-    required this.createdAt,
-    required this.updatedAt,
-  });
+    GetProduct({
+        required this.id,
+        required this.name,
+        required this.userName,
+        required this.userEmail,
+        required this.description,
+        required this.price,
+        required this.imageUrl,
+        required this.createdAt,
+        required this.updatedAt,
+    });
 
-  final String id;
-  final String name;
-  final String description;
-  final String price;
-  final String imageUrl;
-  final String createdAt;
-  final String updatedAt;
+    final String id;
+    final String name;
+    final String userName;
+    final String userEmail;
+    final String description;
+    final String price;
+    final String imageUrl;
+    final String createdAt;
+    final String updatedAt;
 
-  factory GetProduct.fromJson(Map<String, dynamic> json) => GetProduct(
+    factory GetProduct.fromJson(Map<String, dynamic> json) => GetProduct(
         id: json["id"],
         name: json["name"],
+        userName: json["userName"],
+        userEmail: json["userEmail"],
         description: json["description"],
         price: json["price"],
         imageUrl: json["image_url"],
         createdAt: json["created_at"],
         updatedAt: json["updated_at"],
-      );
+    );
+
+    Map<String, dynamic> toJson() => {
+        "id": id,
+        "name": name,
+        "userName": userName,
+        "userEmail": userEmail,
+        "description": description,
+        "price": price,
+        "image_url": imageUrl,
+        "created_at": createdAt,
+        "updated_at": updatedAt,
+    };
 }
