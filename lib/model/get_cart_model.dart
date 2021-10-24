169793ef82333092734752dcbfb56dfb81c69a53
achmadrizkin <arizki.nf02@gmail.com>
2021-10-24 17:03:17 +0700

    a

--- lib/model/get_cart_model.dart
+++ lib/model/get_cart_model.dart
@@ -5,33 +5,53 @@
 import 'dart:convert';
 
 List<GetCart> getCartFromJson(String str) => List<GetCart>.from(json.decode(str).map((x) => GetCart.fromJson(x)));
+
+String getCartToJson(List<GetCart> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
 class GetCart {
     GetCart({
         required this.id,
         required this.name,
+        required this.userName,
+        required this.userEmail,
         required this.description,
         required this.price,
         required this.imageUrl,
-        required this.user,
-        required this.total,
+        required this.createdAt,
+        required this.updatedAt,
     });
 
     final String id;
     final String name;
+    final String userName;
+    final String userEmail;
     final String description;
     final String price;
     final String imageUrl;
-    final String user;
-    final String total;
+    final String createdAt;
+    final String updatedAt;
 
     factory GetCart.fromJson(Map<String, dynamic> json) => GetCart(
         id: json["id"],
         name: json["name"],
+        userName: json["userName"],
+        userEmail: json["userEmail"],
         description: json["description"],
         price: json["price"],
         imageUrl: json["image_url"],
-        user: json["user"],
-        total: json["total"],
+        createdAt: json["created_at"],
+        updatedAt: json["updated_at"],
     );
 
+    Map<String, dynamic> toJson() => {
+        "id": id,
+        "name": name,
+        "userName": userName,
+        "userEmail": userEmail,
+        "description": description,
+        "price": price,
+        "image_url": imageUrl,
+        "created_at": createdAt,
+        "updated_at": updatedAt,
+    };
 }
