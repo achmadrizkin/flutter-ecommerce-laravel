083ae1f4700064825292171bbd71ee97079a4109
achmadrizkin <arizki.nf02@gmail.com>
2021-10-24 10:12:54 +0700

    a

--- lib/pages/search/search.dart
+++ lib/pages/search/search.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce_laravel/utils/color.dart';
 
 class Search extends StatelessWidget {
   const Search({ Key? key }) : super(key: key);
@@ -6,7 +7,7 @@ class Search extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: black,
     );
   }
 }
