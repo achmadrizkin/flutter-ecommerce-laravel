37902fadf977d95f566916c4cf66dc874a3047b7
achmadrizkin <arizki.nf02@gmail.com>
2021-10-25 16:21:30 +0700

    s

---
+++ lib/pages/invoice/invoice_checkout2.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class CheckOut2 extends StatelessWidget {
+  const CheckOut2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("CheckOut"),),
+      body: SafeArea(
+        child: Column(
+          children: [
+
+          ],
+        ),
+      ),
+    );
+  }
+}
