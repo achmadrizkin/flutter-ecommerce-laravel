d23afeab1763834f1b12b0c871e144ece1fba22d
achmadrizkin <arizki.nf02@gmail.com>
2021-10-26 21:13:27 +0700

    l

--- lib/pages/user/user.dart
+++ lib/pages/user/user.dart
@@ -1,14 +1,11 @@
 import 'package:auto_size_text_pk/auto_size_text_pk.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce_laravel/pages/user/widget/user_detail.dart';
-import 'package:flutter_ecommerce_laravel/service/login_controller.dart';
 import 'package:flutter_ecommerce_laravel/utils/color.dart';
 import 'package:flutter_ecommerce_laravel/utils/text_style.dart';
 
 class User extends StatelessWidget {
-  const User({Key? key, required this.model}) : super(key: key);
-
-  final LoginController model;
+  const User({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +18,10 @@ class User extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            UserDetails(screenWidth: screenWidth, screenHeight: screenHeight, models: model,),
+            UserDetails(
+              screenWidth: screenWidth,
+              screenHeight: screenHeight,
+            ),
             SizedBox(height: 10),
             Padding(
               padding: const EdgeInsets.only(left: 10.0, right: 10),
