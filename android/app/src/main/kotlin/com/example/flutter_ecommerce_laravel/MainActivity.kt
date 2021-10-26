d89719c8c94c6b03b0e5a9c5cbe4c95f765d59fb
achmadrizkin <arizki.nf02@gmail.com>
2021-10-26 17:35:07 +0700

    change that for using razorpay

--- android/app/src/main/kotlin/com/example/flutter_ecommerce_laravel/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_ecommerce_laravel/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.flutter_ecommerce_laravel
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
